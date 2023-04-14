1dce376e3935f75921471579e0c450243692b04e
PCA\label <labeldock@me.com>
2023-04-14 23:56:12 +0900

    initial commit

---
+++ app/javascript/controllers/editable_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  click(event) {
+    alert("hi")
+    this.element.setAttribute("contenteditable", "true")
+    this.element.focus()
+  }
+
+  blur(event) {
+    this.element.removeAttribute("contenteditable")
+    this.save()
+  }
+
+  keydown(event) {
+    if (event.keyCode == 13) {
+      event.preventDefault()
+      this.element.removeAttribute("contenteditable")
+    }
+  }
+
+  save() {
+    // TODO
+  }
+}
